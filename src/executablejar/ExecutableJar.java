7e5f1682cfdf8605bd024a6925e0aeccf89aef4f
Michael Scarpace <mscarpace@deloitte.com>
2018-09-07 15:56:04 -0400

    first commit

---
+++ src/executablejar/ExecutableJar.java
@@ -0,0 +1,10 @@
+package executablejar;
+
+import javax.swing.*;
+
+public class ExecutableJar {
+   public static void main(String[] args) {
+      JOptionPane.showMessageDialog(null,"Hey, you guys!");
+      System.exit(0);
+   }
+}
